754fb3ea1388ec5f1afa38891cea29a96feddf35
yasasri <yasasri999@gmail.com>
2025-03-03 14:11:25 -0500

    Added Cart Functionality with a card slider

--- Frontend/src/components/Home/Product.js
+++ Frontend/src/components/Home/Product.js
@@ -1,19 +1,20 @@
-import React from "react";
-
-const productsData = [
+export const products = [
   {
     id: 1,
     name: "I Choose Violence Funny Duck T-Shirt",
     designer: "Tobe Fonseca",
     price: 19.25,
     discount: "25% off",
+    description:
+      "Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.",
+    slug: "tobe-fonseca",
     image:
       "https://ih1.redbubble.net/image.10557653.9817/ssrco,slim_fit_t_shirt,womens,fafafa:ca443f4786,front,square_product,600x600.u7.jpg",
   },
   {
     id: 2,
     name: "Another Cool T-Shirt",
-    designer: "Designer Name",
+    designer: "Designer Jack",
     price: 24.99,
     discount: null,
     image:
@@ -25,8 +26,9 @@ const productsData = [
     designer: "Jane Doe",
     price: 29.99,
     discount: null,
+    slug: "jane-doe",
     image:
-      "https://images.unsplash.com/photo-1600264519827-dfbf9a83f9db?crop=entropy&cs=tinysrgb&fit=max&ixid=MnwzNjk2OXwwfDF8c2VhcmNofDJ8fHRzaGlydHxlbnwwfDB8fHx8&ixlib=rb-1.2.1&q=80&w=400",
+      "https://ih1.redbubble.net/image.2930234064.2990/ssrco,classic_tee,mens,fafafa:ca443f4786,front_alt,square_product,600x600.jpg",
   },
   {
     id: 4,
@@ -34,6 +36,7 @@ const productsData = [
     designer: "Alex Lee",
     price: 19.99,
     discount: null,
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.1621858790.2744/ssrco,classic_tee,mens,fafafa:ca443f4786,front_alt,square_product,600x600.u1.jpg",
   },
@@ -43,6 +46,7 @@ const productsData = [
     designer: "Chris Wright",
     price: 22.99,
     discount: null,
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.1622029562.6354/ssrco,classic_tee,mens,fafafa:ca443f4786,front_alt,square_product,600x600.u1.jpg",
   },
@@ -52,6 +56,7 @@ const productsData = [
     designer: "Michael Kingsley",
     price: 27.99,
     discount: null,
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.1622058681.6989/ssrco,classic_tee,mens,fafafa:ca443f4786,front_alt,square_product,600x600.u1.jpg",
   },
@@ -61,6 +66,7 @@ const productsData = [
     designer: "Sarah Turner",
     price: 21.5,
     discount: null,
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.1622145901.8834/ssrco,classic_tee,womens,fafafa:ca443f4786,front_alt,square_product,600x600.u1.jpg",
   },
@@ -70,6 +76,7 @@ const productsData = [
     designer: "Sophia Adams",
     price: 18.5,
     discount: "15% off",
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.1622145901.8834/ssrco,classic_tee,womens,fafafa:ca443f4786,front_alt,square_product,600x600.u1.jpg",
   },
@@ -79,6 +86,7 @@ const productsData = [
     designer: "Emily Robinson",
     price: 20.99,
     discount: "10% off",
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.1622137993.8673/ssrco,classic_tee,mens,fafafa:ca443f4786,front_alt,square_product,600x600.u1.jpg",
   },
@@ -88,6 +96,7 @@ const productsData = [
     designer: "Olivia Johnson",
     price: 23.99,
     discount: null,
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.2930234064.2990/ssrco,classic_tee,mens,fafafa:ca443f4786,front_alt,square_product,600x600.jpg",
   },
@@ -97,6 +106,7 @@ const productsData = [
     designer: "Ella Brooks",
     price: 25.99,
     discount: null,
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.1622115978.8222/ssrco,classic_tee,mens,fafafa:ca443f4786,front_alt,square_product,600x600.u1.jpg",
   },
@@ -106,6 +116,7 @@ const productsData = [
     designer: "Benjamin Taylor",
     price: 22.49,
     discount: "5% off",
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.5525984223.7880/ssrco,classic_tee,mens,0d162e:9880ee6111,front_alt,square_product,600x600.u9.jpg",
   },
@@ -115,6 +126,7 @@ const productsData = [
     designer: "Nathan Parker",
     price: 19.75,
     discount: null,
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.5525984223.7880/ssrco,classic_tee,mens,0d162e:9880ee6111,front_alt,square_product,600x600.u9.jpg",
   },
@@ -124,6 +136,7 @@ const productsData = [
     designer: "Hannah Scott",
     price: 28.99,
     discount: null,
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.1621807944.1642/ssrco,classic_tee,womens,fafafa:ca443f4786,front_alt,square_product,600x600.u1.jpg",
   },
@@ -133,6 +146,7 @@ const productsData = [
     designer: "Daniel Green",
     price: 24.5,
     discount: null,
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.2651087355.9851/ssrco,classic_tee,mens,fafafa:ca443f4786,front_alt,square_product,600x600.jpg",
   },
@@ -142,6 +156,7 @@ const productsData = [
     designer: "Zara Williams",
     price: 26.5,
     discount: null,
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.5532312550.7577/ssrco,classic_tee,mens,fafafa:ca443f4786,front_alt,square_product,600x600.u6.jpg",
   },
@@ -151,6 +166,7 @@ const productsData = [
     designer: "Benjamin Taylor",
     price: 22.49,
     discount: "5% off",
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.1622130372.8508/ssrco,classic_tee,mens,fafafa:ca443f4786,front_alt,square_product,600x600.u1.jpg",
   },
@@ -160,79 +176,8 @@ const productsData = [
     designer: "Nathan Parker",
     price: 19.75,
     discount: null,
+    slug: "jane-doe",
     image:
       "https://ih1.redbubble.net/image.5444660207.8976/ssrco,classic_tee,womens,531963:70d276b9c6,front_alt,square_product,600x600.u9.jpg",
   },
 ];
-
-const Products = () => {
-  return (
-    <div className="products-container grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 p-6">
-      {productsData.map((product) => (
-        <div
-          key={product.id}
-          className="product group bg-white shadow-lg rounded-lg overflow-hidden relative"
-        >
-          {/* Add to Wishlist Icon */}
-
-          {/* Product Image */}
-          <div className="relative overflow-hidden">
-            <img
-              src={product.image}
-              alt={product.name}
-              className="product-image w-full h-64 object-cover transform transition duration-300 group-hover:scale-110"
-            />
-          </div>
-          <div className="absolute top-2 right-2">
-            <button className="wishlist-btn bg-white p-2 rounded-full shadow-md hover:bg-red-100 transition duration-300">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 text-red-500 hover:text-red-600"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.94l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78v0z"
-                />
-              </svg>
-            </button>
-          </div>
-
-          {/* Product Info */}
-          <div className="product-info p-4">
-            {/* Product Name */}
-            <h3 className="text-lg sm:text-xl md:text-2xl font-semibold truncate">
-              {product.name}
-            </h3>
-
-            {/* Designer Info */}
-            <p className="text-gray-600 text-xs sm:text-sm">
-              {product.designer}
-            </p>
-
-            {/* Price */}
-            <p className="price text-base sm:text-lg md:text-xl text-red-500">
-              ${product.price.toFixed(2)}{" "}
-              {product.discount && (
-                <span className="text-gray-500 text-xs sm:text-sm">
-                  ({product.discount} off)
-                </span>
-              )}
-            </p>
-
-            {/* Add to Cart Button */}
-            <button className="add-to-cart mt-4 bg-blue-500 text-white py-1.5 px-4 sm:py-2 sm:px-5 md:py-2.5 md:px-6 rounded-full hover:bg-blue-600 transition duration-300 text-xs sm:text-sm">
-              Add to Cart
-            </button>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Products;
